a1168faa3a4e344c2e1b568407825f2e642179d3
Gurkan Tongel <gurkantngl@gmail.com>
2023-04-21 20:02:44 +0300

    Son düzenlemeler

--- space_shooter/space_shooter.ino
+++ space_shooter/space_shooter.ino
@@ -5,14 +5,20 @@
 
 #define SSD1306_WIDTH 128
 #define SSD1306_LCDHEIGHT 64
-#define OLED_RESET 8
+#define OLED_RESET 7
+#define LATCH 13
+#define CLK 12
+#define DATA 11
+
 Adafruit_SSD1306 display(SSD1306_WIDTH, SSD1306_LCDHEIGHT, &Wire, OLED_RESET);
 
-const uint8_t MAX_METEORITES = 5;
-const uint8_t MAX_SpaceJunk = 5;
-const uint8_t MAX_BULLETS = 5;
-const uint8_t MAX_PRIZE = 5;
-const uint8_t MAX_IMMUNITY = 5;
+const uint8_t MAX_METEORITES = 1;
+const uint8_t MAX_SpaceJunk = 1;
+const uint8_t MAX_BULLETS = 3;
+const uint8_t MAX_PRIZE = 1;
+const uint8_t MAX_IMMUNITY = 1;
+
+byte digit[10]= {0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x7, 0x7F, 0x6F};
 
 // Meteor
 struct Meteorites {
@@ -41,35 +47,38 @@ struct Immunity{
 };
 
 
-SpaceJunk spacejunk[MAX_SpaceJunk];
-Meteorites meteo[MAX_METEORITES];
+SpaceJunk spacejunk[MAX_SpaceJunk*5];
+Meteorites meteo[MAX_METEORITES*5];
 Bullet fire[MAX_BULLETS];
-Prize prize[MAX_PRIZE];
-Immunity immunity[MAX_IMMUNITY];
+Prize prize[MAX_PRIZE*1];
+Immunity immunity[MAX_IMMUNITY*1];
 
 
 // Global variables
 uint8_t color = WHITE;
-int bullet_button = 2;
+uint8_t color2 = BLACK;
+int bullet_button = 5;
 int buttonState = 0;
 int ship_margin = 10;
 int amor = 3;
 int shot = 3;
 int score = 0;
 int delayTime = 100;
-int LED = 12;
+int time = 0;
+int count1 = 0;
+int count2 = 0;
+int counterS = 0;
 
 // ----------------------------------------------------------
 // Init Functions
 // ----------------------------------------------------------
 void init_display() {
   Serial.begin(9600);
-  // initialize with the I2C addr 0x3D (for the 128x64)
   display.begin(SSD1306_SWITCHCAPVCC, 0x3D);  
   display.display();
   delay(10);
   display.clearDisplay();
-  //display.fillRect(0, 0, 128, 64, SSD1306_WHITE);
+  display.fillRect(0, 0, 128, 64, color2);
 }
 
 // potansiyometre
@@ -82,7 +91,6 @@ void init_pot() {
 // butonlar
 void init_bullet_button(){
   pinMode(bullet_button, INPUT);
-  pinMode(LED, OUTPUT);
 }
 
 // ----------------------------------------------------------
@@ -93,28 +101,28 @@ void display_ship(int pos){
   0x78, 0x00, 0x7c, 0x00, 0x30, 0x00, 0x3f, 0x80, 0xfc, 0xc0, 0xfc, 
   0xc0, 0x3f, 0x80, 0x30, 0x00,0x7c, 0x00, 0x78, 0x00 };
   // Uzay aracı boyutları
-  display.drawBitmap(ship_margin, pos,spaceShip_img, 10, 10, 1);
+  display.drawBitmap(ship_margin, pos,spaceShip_img, 10, 10, color);
 }
 // Uzay aracının potansiyometreye göre hareketi
 int shipPosition(){
-  int potiValue = analogRead(A0);
+  int potiValue = analogRead(A3);
   return map(potiValue, 40, 1024, 0, display.height()-10); 
 }
 
 // ateş etme
 bool fire_bullet() {
   buttonState = digitalRead(bullet_button);
+  Serial.print("buttonState: ");
+  Serial.println(buttonState);
   if (buttonState == 1) {
     if (shot > 0){
-      shot --;
+      shot = shot-1;
       Serial.print("Ates");
       Serial.println(shot);
-      digitalWrite(LED, HIGH);
       return true;
     }
   }
   else if (buttonState == 0){
-    digitalWrite(LED, LOW);
     return false;
   }
 }
@@ -125,49 +133,70 @@ void bullet(bool fire) {
   } 
 }
 
+
+
+
+
 void collition_check(int shipPos) {
   // METEOR ÇARPMASI
-  for (uint8_t i = 0; i < MAX_METEORITES; i++) { 
+  for (uint8_t i = 0; i < MAX_METEORITES*5; i++) { 
     if( meteo[i].x < ship_margin + 8 &&  meteo[i].x >= ship_margin ) {
       // in range of x-axses 
       if( meteo[i].y < shipPos + 8 &&  meteo[i].y >= shipPos ) {
         // in range of y-axses
-        if(amor > 0 ){
-          amor --;          
-        }else {
-          amor = 0;
+        if ((millis() + (count2 * delayTime) - time) > 3000){
+          if(amor > 0 ){
+            digitalWrite(6, HIGH);
+            amor --;          
+          }else {
+            amor = 0;
+          }
+          time = millis() + (count1 * delayTime);
+          count1 = 0;
+          meteo[i].visible = false;
         }
-        meteo[i].visible = false;
       }
     }
   }
   
+
+
+
+
   // UZAY ÇÖPÜ ÇARPMASI
-  for (uint8_t i = 0; i < MAX_SpaceJunk; i++) { 
+  for (uint8_t i = 0; i < MAX_SpaceJunk*5; i++) { 
     if( spacejunk[i].x < ship_margin + 8 &&  spacejunk[i].x >= ship_margin ) {
       // in range of x-axses 
       if( spacejunk[i].y < shipPos + 8 &&  spacejunk[i].y >= shipPos ) {
         // in range of y-axses
-        if(amor > 0 ){
-          amor --;          
-        }else {
-          amor = 0;
+        if((millis() + (count2 * delayTime) - time) > 3000){
+          if(amor > 0 ){
+            digitalWrite(6, HIGH);
+            amor --;          
+          }else {
+            amor = 0;
+          }
+          time = millis() + (count1 * delayTime);
+          count1 = 0;
+          spacejunk[i].visible = false;
         }
-        spacejunk[i].visible = false;
       }
     }
   }
 
 
+
+
   // Ödül Puanı Alma
-  for (uint8_t i = 0; i < MAX_PRIZE; i++) { 
+  for (uint8_t i = 0; i < MAX_PRIZE*5; i++) { 
     if( prize[i].x < ship_margin + 8 &&  prize[i].x >= ship_margin ) {
       // in range of x-axses 
       if( prize[i].y < shipPos + 8 &&  prize[i].y >= shipPos ) {
         // in range of y-axses
         if(amor < 3){
           prize[i].visible = false;
-          amor ++;        
+          amor ++;
+          shot++;     
         }
       }
     }
@@ -178,7 +207,7 @@ void collition_check(int shipPos) {
   
   // UZAY ÇÖPÜ ATEŞ
   for (uint8_t i_bu = 0; i_bu < MAX_BULLETS; i_bu++) {
-    for (uint8_t i_me = 0; i_me < MAX_SpaceJunk; i_me++) { 
+    for (uint8_t i_me = 0; i_me < MAX_SpaceJunk*5; i_me++) { 
       if (fire[i_bu].x >= spacejunk[i_me].x && fire[i_bu].x < spacejunk[i_me].x + 8 && fire[i_bu].fired == true) {
         if (fire[i_bu].y >= spacejunk[i_me].y && fire[i_bu].y < spacejunk[i_me].y + 8 ) {
           spacejunk[i_me].visible = false;
@@ -188,9 +217,11 @@ void collition_check(int shipPos) {
     }
   }
 
+
+
   // METEOR ATEŞ
   for (uint8_t i_bu = 0; i_bu < MAX_BULLETS; i_bu++) {
-    for (uint8_t i_me = 0; i_me < MAX_METEORITES; i_me++) { 
+    for (uint8_t i_me = 0; i_me < MAX_METEORITES*5; i_me++) { 
       if (fire[i_bu].x >= meteo[i_me].x && fire[i_bu].x < meteo[i_me].x + 12 && fire[i_bu].fired == true) {
         if (fire[i_bu].y >= meteo[i_me].y && fire[i_bu].y < meteo[i_me].y + 12 ) {
           fire[i_bu].fired = false;
@@ -204,58 +235,46 @@ void collition_check(int shipPos) {
     }
   }
 
+
+
     // Dokunulmazlık Alma
-  for (uint8_t i_bu = 0; i_bu < MAX_BULLETS; i_bu++) {
-    for (uint8_t i_me = 0; i_me < MAX_IMMUNITY; i_me++) { 
-      if (fire[i_bu].x >= immunity[i_me].x && fire[i_bu].x < immunity[i_me].x + 8 && fire[i_bu].fired == true) {
-        if (fire[i_bu].y >= immunity[i_me].y && fire[i_bu].y < immunity[i_me].y + 8 ) {
-          immunity[i_me].visible = false;
-          fire[i_bu].fired = false;
+  for (uint8_t i = 0; i < MAX_IMMUNITY*1; i++) { 
+    if(immunity[i].x < ship_margin + 8 &&  immunity[i].x >= ship_margin ) {
+      // in range of x-axses 
+      if(immunity[i].y < shipPos + 8 &&  immunity[i].y >= shipPos ) {
+        // in range of y-axses
+          immunity[i].visible = false;
+          time = millis() + (count1*delayTime);
+          count1 = 0; 
         }
       }
     }
-  }
-
-
 }
 
 
-
-void display_lifes(){
-static const unsigned char PROGMEM armor_img[] = {
-  0xFC, // ######..
-  0xFC, // ######..
-  0xFC, // ######..
-  0xFC, // ######..
-  0x78, // .####...
-  0x30, // ..##....
-};
-  
-  display.setTextColor(color);
-  display.setTextSize(1);
-  display.setCursor(display.width() - 20, 0);
-  display.println(amor);
-  display.drawBitmap(display.width() - 30, 0, armor_img , 6, 6, 1);
-}
 
 void display_score(){
-  display.setTextColor(color);
-  display.setTextSize(1);
-  display.setCursor(10, 0);
-  display.println(score);
-  
+      digitalWrite(LATCH, LOW);
+      shiftOut(DATA, CLK, MSBFIRST, ~digit[score%10]);  
+      shiftOut(DATA, CLK, MSBFIRST, ~digit[(score/10)%10]); 
+      shiftOut(DATA, CLK, MSBFIRST, ~digit[score/100]); 
+      digitalWrite(LATCH, HIGH);
+
 }
 
 
 
 
 //METEOR
-
-
 void init_meteo() {
-  for (uint8_t i = 0; i < MAX_METEORITES; i++) {
-    meteo[i].x = 120;
-    meteo[i].y = random(display.height()-8);
+  int x = 60;
+  for (uint8_t i = 0; i < MAX_METEORITES*5; i++) {
+    meteo[i].x = x;
+    meteo[i].y = random(display.height()-8);;
+    if(i%MAX_METEORITES==0){
+      x += 32;
+    }
+    //meteo[i].y = random(display.height()-8);
     meteo[i].visible = true;
   }
 }
@@ -263,18 +282,18 @@ void init_meteo() {
 
 
 void draw_meteo() {
-  for (uint8_t i = 0; i < MAX_METEORITES; i++) {
+  for (uint8_t i = 0; i < MAX_METEORITES*5; i++) {
     static const unsigned char PROGMEM meteo_img[] = {
-      0x00,0x00, // ................
-      0x3F,0x00, // ..######........
-      0x7F,0x80, // .########.......
-      0xE7,0x80, // ###..####.......
-      0xE7,0x80, // ###..####.......
-      0xFF,0x80, // #########.......
-      0x7F,0x80, // .########.......
-      0x7F,0x80, // .########.......
-      0x3F,0x00, // ..######........
-      0x3E,0x00 // ..#####.........
+      0x00,0x00,
+      0x3F,0x00,
+      0x7F,0x80,
+      0xE7,0x80,
+      0xE7,0x80,
+      0xFF,0x80,
+      0x7F,0x80,
+      0x7F,0x80, 
+      0x3F,0x00, 
+      0x3E,0x00 
     };
     if(meteo[i].visible == true ){
       display.drawBitmap(meteo[i].x, meteo[i].y, meteo_img, 10, 10, color);
@@ -283,11 +302,12 @@ void draw_meteo() {
 }
 
 void move_meteo() {
-  for (uint8_t i = 0; i < MAX_METEORITES; i++) {
+  for (uint8_t i = 0; i < MAX_METEORITES*5; i++) {
     meteo[i].x -= 8;
     if (meteo[i].x < 0) {
       meteo[i].x = 120;
-      meteo[i].y = random(display.height());
+      meteo[i].y = random(display.height());;
+      //meteo[i].y = random(display.height());
       meteo[i].visible = true;
     }
   }
@@ -300,9 +320,13 @@ void move_meteo() {
 //
 
 void init_spaceJunk() {
-  for (uint8_t i = 0; i < MAX_SpaceJunk; i++) {
-    spacejunk[i].x = 120;
+  int x = 40;
+  for (uint8_t i = 0; i < MAX_SpaceJunk*5; i++) {
+    spacejunk[i].x = x;
     spacejunk[i].y = random(display.height()-8);
+    if(i%MAX_SpaceJunk==0){
+      x += 32;
+    }
     spacejunk[i].visible = true;
   }
 }
@@ -310,7 +334,7 @@ void init_spaceJunk() {
 
 
 void draw_spaceJunk() {
-  for (uint8_t i = 0; i < MAX_SpaceJunk; i++) {
+  for (uint8_t i = 0; i < MAX_SpaceJunk*5; i++) {
     if(spacejunk[i].visible == true ){
       display.drawRect(spacejunk[i].x, spacejunk[i].y, 8, 8, color);
     }
@@ -320,15 +344,17 @@ void draw_spaceJunk() {
 
 
 void move_spaceJunk() {
-  for (uint8_t i = 0; i < MAX_SpaceJunk; i++) {
+  for (uint8_t i = 0; i < MAX_SpaceJunk*5; i++) {
     spacejunk[i].x -= 8;
     if (spacejunk[i].x < 0) {
-      spacejunk[i].x = display.width();
-      spacejunk[i].y = random(display.height());
+      spacejunk[i].x = 120;
+      spacejunk[i].y = random(display.height());;
       spacejunk[i].visible = true;
     }
   }
 }
+
+
 
 
 //
@@ -380,16 +406,20 @@ void draw_bullet() {
 //
 
 void init_prize() {
-  for (uint8_t i = 0; i < MAX_METEORITES; i++) {
-    meteo[i].x = 120;
+  int x = 80;
+  for (uint8_t i = 0; i < MAX_PRIZE*1; i++) {
+    meteo[i].x = x;
     meteo[i].y = random(display.height()-8);
     meteo[i].visible = true;
+    if(i%MAX_PRIZE==0){
+      x += 16;
+    }
   }
 }
 
 
 void draw_prize(){
-  for (uint8_t i = 0; i < MAX_PRIZE; i++) {
+  for (uint8_t i = 0; i < MAX_PRIZE*1; i++) {
     static const unsigned char PROGMEM armor_img[] = {
       0xFC, // ######..
       0xFC, // ######..
@@ -405,10 +435,10 @@ void draw_prize(){
 
 }
 void move_prize() {
-  for (uint8_t i = 0; i < MAX_PRIZE; i++) {
+  for (uint8_t i = 0; i < MAX_PRIZE*1; i++) {
     prize[i].x -= 8;
     if (prize[i].x < 0) {
-      prize[i].x = display.width();
+      prize[i].x = 120;
       prize[i].y = random(display.height());
       prize[i].visible = true;
     }
@@ -423,30 +453,34 @@ void move_prize() {
 //
 
 void init_immunity() {
-  for (uint8_t i = 0; i < MAX_IMMUNITY; i++) {
-    immunity[i].x = 120;
+  int x = 40;
+  for (uint8_t i = 0; i < MAX_IMMUNITY*1; i++) {
+    immunity[i].x = x;
     immunity[i].y = random(display.height()-8);
     immunity[i].visible = true;
+    if(i%MAX_IMMUNITY==0){
+      x += 16;
+    }
   }
 }
 
 
 
 void draw_immunity() {
-  for (uint8_t i = 0; i < MAX_IMMUNITY; i++) {
+  for (uint8_t i = 0; i < MAX_IMMUNITY*1; i++) {
     if(immunity[i].visible == true ){
       display.drawTriangle(immunity[i].x, immunity[i].y, immunity[i].x+8, immunity[i].y, immunity[i].x, immunity[i].y+8, color);
     }
-    }
+  }
 }
 
 
 
 void move_immunity() {
-  for (uint8_t i = 0; i < MAX_IMMUNITY; i++) {
+  for (uint8_t i = 0; i < MAX_IMMUNITY*1; i++) {
     immunity[i].x -= 8;
     if (immunity[i].x < 0) {
-      immunity[i].x = display.width();
+      immunity[i].x = 120;
       immunity[i].y = random(display.height());
       immunity[i].visible = true;
     }
@@ -467,6 +501,17 @@ void move_immunity() {
 // SETUP
 // ----------------------------------------------------------
 void setup() {
+  pinMode(LATCH, OUTPUT);
+  pinMode(CLK, OUTPUT);
+  pinMode(DATA, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(A1, INPUT);
+  pinMode(8, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(0, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(2, OUTPUT);
   init_display();
   init_pot();
   
@@ -475,11 +520,9 @@ void setup() {
   
   init_bullet_button();
   init_bullet();
-
+  init_immunity();
   init_prize();
-  
 }
-
 
 
 
@@ -487,11 +530,23 @@ void setup() {
 // LOOP 
 // ----------------------------------------------------------
 void loop() {
-  // yıldızları hareket ettir
-  move_spaceJunk();
+
 
   // yıldızları çiz
   draw_spaceJunk();
+  // kurşunu çiz
+  draw_bullet();
+  //meteoru çiz
+  draw_meteo();
+  //ödül puanı
+  draw_prize();
+  //dokunulmazlık
+  draw_immunity();
+  
+  // yıldızları hareket ettir
+  move_spaceJunk();
+
+  
   
   // uzay gemisi
   display_ship(shipPosition());
@@ -499,47 +554,119 @@ void loop() {
   // kurşun
   bullet(fire_bullet());
 
-  // kurşunu çiz
-  draw_bullet();
+  
   // kurşunu hareket ettir
   move_bullet();
 
-  //meteoru çiz
-  draw_meteo();
+  
   // meteoru hareket ettir
   move_meteo();
 
-  //ödül puanı
-  draw_prize();
+  
   move_prize();
 
+  
+  move_immunity();
   
   // çarpışma kontrolü
   collition_check(shipPosition());
   
-  // can
-  display_lifes();
 
   // skor
   display_score();
+  
+  if (amor >= 3){
+    digitalWrite(10, HIGH);
+    digitalWrite(9, HIGH);
+    digitalWrite(8, HIGH);
+  }
+  
+  if (amor == 2){
+    digitalWrite(10, HIGH);
+    digitalWrite(9, HIGH);
+    digitalWrite(8, LOW);
+  }
+
+  if (amor == 1){
+    digitalWrite(10, HIGH);
+    digitalWrite(9, LOW);
+    digitalWrite(8, LOW);
+  }
 
   if (amor == 0){
+    digitalWrite(10, LOW);
+    digitalWrite(9, LOW);
+    digitalWrite(8, LOW);
     lose();
   }  
-  
+
+  if (shot >= 3){
+    digitalWrite(0, HIGH);
+    digitalWrite(3, HIGH);
+    digitalWrite(2, HIGH);
+  }
+
+
+  if(shot == 2){
+    digitalWrite(0, HIGH);
+    digitalWrite(2, HIGH);
+    digitalWrite(3, LOW);
+  }
+
+  if(shot == 1){
+    digitalWrite(0, HIGH);
+    digitalWrite(2, LOW);
+    digitalWrite(3, LOW);
+  }
+
+  if(shot == 0){
+    digitalWrite(0, LOW);
+    digitalWrite(2, LOW);
+    digitalWrite(3, LOW);
+  }
+
+  int sensorValue = analogRead(A0); // LDR Sensor
+
+  if (sensorValue > 15){
+    color = BLACK;
+    color2 = WHITE;
+  }else{
+    color = WHITE;
+    color2 = BLACK;
+  }
+  Serial.println(sensorValue);
+
   display.display();
   delay(delayTime);
+  count1++;
+  count2++;
   score ++;
+  if(score%15 == 0){
+    if(shot<3){
+      shot++;
+    }
+  }
+  if(score%25 == 0){
+    if(shot<3){
+      shot++;
+    }
+    if(amor<3){
+      amor++;
+    }
+  }  
+  digitalWrite(6, LOW);
   display.clearDisplay();
-  //display.fillRect(0, 0, 128, 64, SSD1306_WHITE);
+  display.fillRect(0, 0, 128, 64, color2);
 }
 
 
 void lose(){
   display.clearDisplay();
+  display.fillRect(0, 0, 128, 64, color2);
   display.setTextSize(2);
   display.setTextColor(color);
   display.setRotation(1);
+  digitalWrite(6, LOW);
   display.setCursor(4, 64);
   display.println("LOSE!");  
   display.display();
